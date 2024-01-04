e2f1696fe349bd997226805837bb28ed1a2db328
renatotheo <renatotheobaldo@gmail.com>
2024-01-04 12:05:50 -0300

    Incremente do fluxo do app com o logout, juntamente com algumas verificações de persistencia.

--- app/src/main/java/com/example/signin/LoginActivity.kt
+++ app/src/main/java/com/example/signin/LoginActivity.kt
@@ -1,13 +1,20 @@
 // LoginActivity.kt
 package com.example.signin
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
@@ -19,20 +26,69 @@ import com.google.firebase.auth.FirebaseUser
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var textViewErrorMessage: TextView  // Adicione esta linha
+    private lateinit var textViewErrorMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         auth = FirebaseAuth.getInstance()
-        textViewErrorMessage = findViewById(R.id.textViewErrorMessage)  // Adicione esta linha
+        textViewErrorMessage = findViewById(R.id.textViewErrorMessage)
+
+        // Verifica se já existe um usuário autenticado
+        val currentUser = auth.currentUser
+        val sharedPreferences = getSharedPreferences("MY_APP_PREFERENCES", Context.MODE_PRIVATE)
+        val isUserLogged = sharedPreferences.getBoolean("IS_USER_LOGGED_IN", false)
+
+
+        if (currentUser != null) {
+            // Se existe, direcione para a WelcomeActivity
+            goToWelcomeActivity() // Finaliza a LoginActivity para que o usuário não possa voltar pressionando o botão "Back"
+        }
+        /*if (currentUser != null && isUserLogged) {
+            // Se já estiver autenticado e a flag indica que o usuário está logado, vá para a tela de boas-vindas
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish() // Finalize a LoginActivity para que não possa ser acessada pressionando Voltar
+        }*/
 
         val btnLogin: Button = findViewById(R.id.btnLogin)
+        val btnSignUp: Button = findViewById(R.id.btnSignUp)
+
+        // Adicionando o toggle de visibilidade de senha
+        val imageViewPasswordVisibility: ImageView = findViewById(R.id.imageViewPasswordVisibility)
+        val editTextPassword: EditText = findViewById(R.id.editTextLoginPassword)
+
+        imageViewPasswordVisibility.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Mostra a senha enquanto o botão está pressionado
+                    togglePasswordVisibility(editTextPassword, imageViewPasswordVisibility)
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    // Volta à forma de senha padrão quando o botão é solto ou cancelado
+                    togglePasswordVisibility(editTextPassword, imageViewPasswordVisibility)
+                }
+            }
+            true
+        }
+
         btnLogin.setOnClickListener {
             Log.d("LoginActivity", "Botão de login clicado")
             login()
         }
+
+        btnSignUp.setOnClickListener {
+            // Adicionar código para abrir a tela de cadastro
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+    private fun goToWelcomeActivity() {
+        val intent = Intent(this, WelcomeActivity::class.java)
+        startActivity(intent)
+        finish() // Finaliza a LoginActivity para que o usuário não possa voltar pressionando o botão "Back"
     }
 
     private fun login() {
@@ -41,6 +97,12 @@ class LoginActivity : AppCompatActivity() {
 
         val email = editTextEmail.text.toString()
         val password = editTextPassword.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            // Se o e-mail ou a senha estiverem vazios, exiba uma mensagem de erro
+            showError("Por favor, preencha todos os campos.")
+            return
+        }
 
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
@@ -96,6 +158,46 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
     }
+
+
+    // Método para lidar com o clique no link "Esqueceu sua senha?"
+    fun forgotPassword(view: View) {
+        val emailEditText: EditText = findViewById(R.id.editTextLoginEmail)
+        val email = emailEditText.text.toString()
+
+        if (email.isNotEmpty()) {
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Email de redefinição de senha enviado com sucesso
+                        Toast.makeText(this, "Um e-mail de redefinição de senha foi enviado para $email", Toast.LENGTH_LONG).show()
+                    } else {
+                        // Falha ao enviar o e-mail de redefinição de senha
+                        showError("Falha ao enviar o e-mail de redefinição de senha. Verifique o endereço de e-mail.")
+                    }
+                }
+        } else {
+            // Campo de e-mail vazio
+            showError("Por favor, insira seu endereço de e-mail.")
+        }
+    }
+
+    private fun togglePasswordVisibility(editTextPassword: EditText, imageViewPasswordVisibility: ImageView) {
+        // Adicione aqui o código para alternar entre visibilidade e invisibilidade de senha
+        if (editTextPassword.transformationMethod == PasswordTransformationMethod.getInstance()) {
+            // Se a senha estiver oculta, mostra a senha
+            editTextPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            imageViewPasswordVisibility.setImageResource(R.drawable.ic_visibility_on)
+        } else {
+            // Se a senha estiver visível, oculta a senha
+            editTextPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+            imageViewPasswordVisibility.setImageResource(R.drawable.ic_visibility_off)
+        }
+
+        // Move o cursor para o final do texto
+        editTextPassword.setSelection(editTextPassword.text.length)
+    }
+
 
     private fun showError(message: String) {
         Log.d("LoginActivity", "Mostrando erro: $message")
--- app/src/main/java/com/example/signin/WelcomeActivity.kt
+++ app/src/main/java/com/example/signin/WelcomeActivity.kt
@@ -1,17 +1,49 @@
 package com.example.signin
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private var isUserLoggedIn: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
+        auth = FirebaseAuth.getInstance()
+
         // Recupere a mensagem de saudação do layout
         val welcomeMessage: TextView = findViewById(R.id.textViewWelcomeMessage)
+        val btnLogout: Button = findViewById(R.id.btnLogout)
+
+
+
         welcomeMessage.text = "Olá, bem-vindo!"
+
+        btnLogout.setOnClickListener {
+            logout()
+        }
     }
+    private fun logout() {
+        auth.signOut()
+
+        // Limpar a persistência local (exemplo usando SharedPreferences)
+        val sharedPreferences = getSharedPreferences("MY_APP_PREFERENCES", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.clear()
+        editor.putBoolean("IS_USER_LOGGED_IN", false)  // Defina a flag como false
+        editor.apply()
+
+        // Vá para a tela de login e limpe a pilha de atividades anteriores
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
 }
